deef487b1f80008653dd55f47695dfabc3b90a4e
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-18 17:15:02 -0500

    add consts of site

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Dunor Boutique';
+export const SITE_DESCRIPTION = 'Toda ocasión especial merece un atuendo excepcional, y en Dunor Boutique podrás elevarla con elegancia y estilo.';
