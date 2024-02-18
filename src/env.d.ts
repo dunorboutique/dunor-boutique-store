9a164ace6d62b26f638e0b15cf31b4b620356979
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-18 16:57:30 -0500

    add Cloudinary service for image serving

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,9 @@
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string
   readonly SUPABASE_ANON_KEY: string
+  readonly CLOUDINARY_CLOUD_NAME: string
+  readonly CLOUDINARY_API_KEY: string
+  readonly CLOUDINARY_API_SECRET: string
 }
 
 interface ImportMeta {
