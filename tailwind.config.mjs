d8c47519f2386ace421757f3241762d6ddd27141
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-18 19:26:54 -0500

    add custom sizes and fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        "dunor-black": "#212121"
+      },
+      screens: {
+        "2xsmall": "320px",
+        xsmall: "512px",
+      },
+      fontSize: {
+        "3xl": "2rem"
+      },
+      fontFamily: {
+        display: ["Playfair Display", "sans-serif"],
+        sans: ["Josefin Sans", "sans-serif"]
+      }
+    }
   },
   plugins: []
 }
