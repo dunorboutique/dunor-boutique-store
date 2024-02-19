7dbd44d4431dd9051dcd48dac24fa6058259625a
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-18 23:06:57 -0500

    Add sitemap integration and redirects to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from "astro/config"
 import vercel from "@astrojs/vercel/serverless"
 import tailwind from "@astrojs/tailwind"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.dunorboutique.net/",
-  output: "hybrid",
+  output: "server",
   adapter: vercel(),
-  integrations: [tailwind()]
+  redirects: {
+    "/categories": "/"
+  },
+  integrations: [tailwind(), sitemap()]
 })
