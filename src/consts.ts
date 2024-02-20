df876fddf43531fd373076a8182054913545df64
Jesus Borrero <jesufer2018@hotmail.com>
2024-02-19 19:10:37 -0500

    Add contact information and social links to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,14 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Dunor Boutique';
-export const SITE_DESCRIPTION = 'Toda ocasión especial merece un atuendo excepcional, y en Dunor Boutique podrás elevarla con elegancia y estilo.';
+export const SITE_TITLE = "Dunor Boutique"
+export const SITE_DESCRIPTION =
+  "Toda ocasión especial merece un atuendo excepcional, y en Dunor Boutique podrás elevarla con elegancia y estilo."
+export const SITE_TELEPHONE = "576053856181"
+export const SITE_WHATSAPP = "573216105383"
+export const SITE_SOCIAL_LINKS = {
+  whatsapp: `https://wa.me/${SITE_WHATSAPP}?text=Hola, me gustaría saber más sobre sus servicios`,
+  tiktok: "https://tiktok.com/@dunor_boutique",
+  facebook: "https://www.facebook.com/dunorboutique",
+  instagram: "https://www.instagram.com/dunorboutique"
+}
