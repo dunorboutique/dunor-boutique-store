bd121b8a6e1bc616d685440847bfe7b27f63077e
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-03 01:36:32 -0500

    Add fileToBase64 function to format.ts

--- src/lib/format.ts
+++ src/lib/format.ts
@@ -9,3 +9,18 @@ export const formatPrice = (price: number) =>
 
 export const formatPhoneNumber = (phoneNumber: string) =>
   phoneNumber.replace(/(\d{2})(\d{3})(\d{3})(\d{4})/, "+$1 ($2) $3-$4")
+
+export function fileToBase64(file: File) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => {
+      let encoded = reader.result?.toString().replace(/^data:(.*,)?/, "") ?? ""
+      if (encoded.length % 4 > 0) {
+        encoded += "=".repeat(4 - (encoded.length % 4))
+      }
+      resolve(encoded)
+    }
+    reader.onerror = (error) => reject(error)
+  })
+}
