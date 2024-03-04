2cea742e31bf0b7c25919d7dc8619d805083403b
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-04 02:24:21 -0500

    Add FormField component

---
+++ src/components/ui/FormField.tsx
@@ -0,0 +1,67 @@
+import { InfoIcon } from "@icons/Info"
+
+interface FormFieldProps {
+  type: "number" | "file" | "select"
+  label: string
+  name: string
+  min?: number
+  max?: number
+  accept?: string
+  tooltip?: string
+  className?: string
+  placeholder?: string
+  description?: string
+  children?: preact.ComponentChildren
+}
+
+export function FormField({
+  label,
+  description,
+  type,
+  name,
+  min,
+  max,
+  accept,
+  placeholder,
+  tooltip,
+  className,
+  children
+}: FormFieldProps) {
+  return (
+    <label class="flex flex-col gap-2 w-full">
+      <span class="text-lg font-medium">{label}</span>
+      {type === "select" ? (
+        <select
+          name={name}
+          class="block px-3 py-2 w-full bg-white border rounded-md border-neutral-300 placeholder-neutral-500 focus-visible:outline-none focus-visible:ring-dunor-gold focus-visible:ring-4 transition-shadow"
+          required
+        >
+          <option value="">-- Selecciona una opción --</option>
+          {children}
+        </select>
+      ) : (
+        <input
+          type={type}
+          name={name}
+          min={min}
+          max={max}
+          accept={accept}
+          placeholder={placeholder}
+          className={`block px-3 py-2 w-full bg-white border rounded-md border-neutral-300 placeholder-neutral-500 focus-visible:outline-none focus-visible:ring-dunor-gold focus-visible:ring-4 transition-shadow placeholder:font-[sans-serif] ${className}`}
+          required
+        />
+      )}
+      {description && (
+        <span
+          aria-label={tooltip || label}
+          className={`flex items-center gap-0.5 text-neutral-500 text-sm leading-none cursor-help hover:underline decoration-wavy ${
+            tooltip && "hint--bottom hint--large hint--rounded"
+          }`}
+        >
+          <InfoIcon />
+          {description}
+        </span>
+      )}
+    </label>
+  )
+}
