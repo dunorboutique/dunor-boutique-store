3a0b0d5518e51ca076cc6464f6346b6d25fd46fa
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-04 02:49:00 -0500

    Refactor useFetch hook to allow updating URL and options

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -4,14 +4,23 @@ type FetchState<T> = {
   data: T | null
   error: Error | null
   isLoading: boolean
+  updateFetch: (url: string, options: RequestInit) => void
 }
 
-export function useFetch<T>(url: string, options?: RequestInit): FetchState<T> {
+export function useFetch<T>(initialUrl: string, initOptions?: RequestInit): FetchState<T> {
+  const [url, updateUrl] = useState(initialUrl)
+  const [options, setOptions] = useState<RequestInit>(initOptions ?? {})
   const [data, setData] = useState<T | null>(null)
   const [error, setError] = useState<Error | null>(null)
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
+  function updateFetch(url: string, options: RequestInit) {
+    updateUrl(url)
+    setOptions(options)
+  }
+
   useEffect(() => {
+    if (!url) return
     async function fetchData() {
       setIsLoading(true)
       try {
@@ -19,7 +28,7 @@ export function useFetch<T>(url: string, options?: RequestInit): FetchState<T> {
         if (!response.ok) {
           throw new Error("Network response was not ok")
         }
-        const result = await response.json()
+        const result = await response.json() as T
         setData(result)
       } catch (error: any) {
         setError(error)
@@ -29,7 +38,7 @@ export function useFetch<T>(url: string, options?: RequestInit): FetchState<T> {
     }
 
     fetchData()
-  }, [url])
+  }, [url, options])
 
-  return { data, error, isLoading }
+  return { data, error, isLoading, updateFetch }
 }
