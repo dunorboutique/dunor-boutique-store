9867e06ca0fb7cbf6cb286b935a19b2f4d6057d5
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-04 01:10:30 -0500

    Add new types for cart items and Bodygram API response and request data

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,46 @@ export type Product = {
 export type CartItem = Product & {
   quantity: number
 }
+
+export type PartialCartItem = Pick<Product, 'id' | 'price'> & {
+  quantity: number
+}
+
+export type BodygramResponse = {
+  entry: {
+    avatar: {
+      data: string
+      format: "obj"
+      type: "highResolution"
+    },
+    createdAt: number
+    customScanId: string
+    error: {
+      code: string
+    }
+    id: string
+    input: {
+      photoScan: {
+        age: number
+        gender: string
+        height: number
+        weight: number
+      }
+    }
+    measurements: Array<{
+      name: string
+      unit: "mm"
+      value: number
+    }>
+    status: string
+  }
+}
+
+export type BodygramRequestData = {
+  age: number
+  weight: number
+  height: number
+  gender: string
+  frontPhoto: string
+  rightPhoto: string
+}
