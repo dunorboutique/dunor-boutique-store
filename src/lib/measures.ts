80e938d88761ebb8e5c82daabbab56405ef65676
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-05 01:25:06 -0500

    Add filterByMeasureName function to filter and transform measurements

--- src/lib/measures.ts
+++ src/lib/measures.ts
@@ -1,3 +1,5 @@
+import type { BodygramResponse } from "@types"
+
 export const FILTERED_MEASUREMENTS = [
   "waistGirth",
   "bustGirth",
@@ -19,4 +21,13 @@ export const TRANSLATED_MEASUREMENTS = {
 export type MeasurementName = keyof typeof TRANSLATED_MEASUREMENTS
 export function measureNameToTitle(key: MeasurementName) {
   return TRANSLATED_MEASUREMENTS[key] ?? key
+}
+
+export function filterByMeasureName(measurements: BodygramResponse["entry"]["measurements"]) {
+  return measurements
+    .filter(measurement => FILTERED_MEASUREMENTS.includes(measurement.name))
+    .map(measurement => ({
+      name: measurement.name,
+      value: Number(measurement.value) / 10
+    }))
 }
