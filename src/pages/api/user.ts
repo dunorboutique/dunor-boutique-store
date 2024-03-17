9f43289bf760fde27b7ceb7686cbf8ef49b5d759
Jesus Borrero <jesufer2018@hotmail.com>
2024-03-16 19:51:23 -0500

    Add validation for required fields in POST API route

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -4,10 +4,19 @@ import { createUserMeasures } from "@db/measures"
 export const POST: APIRoute = async ({ request }) => {
   const body = await request.json()
   const { userId: user_id, measures, userInfo: user_info } = body
+  if (!user_id) {
+    return new Response("User id is required", { status: 400 })
+  }
+  if (!measures) {
+    return new Response("Measures are required", { status: 400 })
+  }
+  if (!user_info) {
+    return new Response("User info is required", { status: 400 })
+  }
+
   const userMeasure = { user_id, measures, user_info }
   const { error } = await createUserMeasures(userMeasure)
   if (error) {
-    console.error("Error al guardar las medidas", error)
     return new Response("Error al guardar las medidas", { status: 500 })
   }
   return new Response("Medidas guardadas", { status: 200 })
