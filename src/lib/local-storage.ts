b51aa47a8f60e5791e35524e7ebbf76a79dd9d69
Dunor Boutique <boutiquedunor@gmail.com>
2024-04-16 00:51:48 -0500

    Add local storage utility functions

---
+++ src/lib/local-storage.ts
@@ -0,0 +1,12 @@
+export function getItem<T>(key: string): T | null {
+  const item = localStorage.getItem(key)
+  return item ? JSON.parse(item) : null
+}
+
+export function setItem<T>(key: string, value: T): void {
+  localStorage.setItem(key, JSON.stringify(value))
+}
+
+export function removeItem(key: string): void {
+  localStorage.removeItem(key)
+}
